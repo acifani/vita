a31e8d333702f04025d6b7c39ed912c2c2600327
deadprogram <ron@hybridgroup.com>
2023-11-14 16:58:01 +0100

    lib/game: add universe.Parse()
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- lib/game/errors.go
+++ lib/game/errors.go
@@ -5,6 +5,7 @@ import (
 )
 
 var (
-	errInvalidLength = errors.New("slice does not match universe size")
-	errInvalidID     = errors.New("IDs cannot be the same")
+	errInvalidLength    = errors.New("slice does not match universe size")
+	errInvalidID        = errors.New("IDs cannot be the same")
+	errInvalidCharacter = errors.New("cannot parse invalid character")
 )
